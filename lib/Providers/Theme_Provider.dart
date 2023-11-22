8c500cf8284507dd1662fbc88f787a82f69b669b
IVIahdi <mahdi.11231@gmail.com>
2023-11-23 01:25:14 +0300

    test

---
+++ lib/Providers/Theme_Provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = ThemeData.light();
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+  ThemeData get themeData => _themeData;
+  // Test
+
+  void toggleTheme() {
+    if (_themeData == ThemeData.light()) {
+      _themeData = ThemeData.dark();
+      _isDarkMode = !_isDarkMode;
+    } else {
+      _themeData = ThemeData.light();
+      _isDarkMode = !_isDarkMode;
+    }
+    notifyListeners();
+  }
+}
