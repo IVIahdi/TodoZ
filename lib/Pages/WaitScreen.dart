bb74886b6490e4c889d677c17563606faf4d2247
IVIahdi <mahdi.11231@gmail.com>
2023-11-23 07:51:14 +0300

    updates

---
+++ lib/Pages/WaitScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class WaitPage extends StatefulWidget {
+  const WaitPage({super.key});
+
+  @override
+  State<WaitPage> createState() => _WaitPageState();
+}
+
+class _WaitPageState extends State<WaitPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'Login/WB.dart';
 import 'Providers/Theme_Provider.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDQPWAPr-S8yHLtIRN6AryveH5ln9hZMcE",
+          appId: "1:562891858605:web:30946af5214569d119dc24",
+          messagingSenderId: "562891858605",
+          projectId: "todoz-d4cf7"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
