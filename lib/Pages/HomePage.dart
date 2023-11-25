3f030bded79e2c1fd8146a0b2a9898c9e936a20e
IVIahdi <mahdi.11231@gmail.com>
2023-11-25 14:45:00 +0300

    IVI

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,8 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:listify/Login/WB.dart';
 import 'package:provider/provider.dart';
-
+import '../Login/WB.dart';
 import '../Providers/Theme_Provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,19 +17,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _newTodoController = TextEditingController();
+
   void _logout() async {
     await FirebaseAuth.instance.signOut();
     Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => WelcomeBack()));
+        context, MaterialPageRoute(builder: (context) => const WelcomeBack()));
+  }
+
+  Future<void> _addTodo() async {
+    final String newTodo = _newTodoController.text.trim();
+    if (newTodo.isNotEmpty) {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.user.user!.uid)
+          .update({
+        'todos': FieldValue.arrayUnion([newTodo]),
+      });
+      _newTodoController.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final _user = widget.user.user;
+    final user = widget.user.user;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome, ${widget.userData?['username'] ?? _user?.uid}!'),
+        title: Text('Welcome, ${widget.userData?['username'] ?? user?.uid}!'),
         actions: [
           IconButton(
             icon: const Icon(Icons.brightness_4),
@@ -38,17 +53,64 @@ class _HomePageState extends State<HomePage> {
             },
           ),
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
             onPressed: _logout,
           ),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: StreamBuilder<DocumentSnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(widget.user.user!.uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final userData =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    final todos = userData['todos'] ?? [];
+                    return ListView.builder(
+                      itemCount: todos.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(todos[index]),
+                          subtitle: Text('By: ${userData['username']}'),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _newTodoController,
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _addTodo,
+                  child: Text('Add Todo'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/Providers/Theme_Provider.dart
+++ lib/Providers/Theme_Provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeData _themeData = ThemeData.from(colorScheme: const ColorScheme.light());
